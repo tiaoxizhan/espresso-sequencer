2cca235bdc4f07ac0857da1ba5ffb5ef41814340
pls148 <pls148@users.noreply.github.com>
2025-02-24 01:12:30 +0000

    deploy: 1eeb6e3708c3dcd9a0a869e6226f41b64fc53050

--- trait.impl/ark_serialize/trait.CanonicalDeserialize.js
+++ trait.impl/ark_serialize/trait.CanonicalDeserialize.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["espresso_types",[["impl CanonicalDeserialize for <a class=\"struct\" href=\"espresso_types/v0/v0_1/fee_info/struct.FeeAccount.html\" title=\"struct espresso_types::v0::v0_1::fee_info::FeeAccount\">FeeAccount</a>"],["impl CanonicalDeserialize for <a class=\"struct\" href=\"espresso_types/v0/v0_1/fee_info/struct.FeeAmount.html\" title=\"struct espresso_types::v0::v0_1::fee_info::FeeAmount\">FeeAmount</a>"],["impl CanonicalDeserialize for <a class=\"struct\" href=\"espresso_types/v0/v0_1/fee_info/struct.FeeInfo.html\" title=\"struct espresso_types::v0::v0_1::fee_info::FeeInfo\">FeeInfo</a>"],["impl CanonicalDeserialize for <a class=\"struct\" href=\"espresso_types/v0/v0_1/transaction/struct.NamespaceId.html\" title=\"struct espresso_types::v0::v0_1::transaction::NamespaceId\">NamespaceId</a>"],["impl CanonicalDeserialize for <a class=\"struct\" href=\"espresso_types/v0/v0_1/transaction/struct.Transaction.html\" title=\"struct espresso_types::v0::v0_1::transaction::Transaction\">Transaction</a>"]]]]);
+    var implementors = Object.fromEntries([["espresso_types",[["impl CanonicalDeserialize for <a class=\"struct\" href=\"espresso_types/v0/v0_1/struct.FeeAccount.html\" title=\"struct espresso_types::v0::v0_1::FeeAccount\">FeeAccount</a>"],["impl CanonicalDeserialize for <a class=\"struct\" href=\"espresso_types/v0/v0_1/struct.FeeAmount.html\" title=\"struct espresso_types::v0::v0_1::FeeAmount\">FeeAmount</a>"],["impl CanonicalDeserialize for <a class=\"struct\" href=\"espresso_types/v0/v0_1/struct.FeeInfo.html\" title=\"struct espresso_types::v0::v0_1::FeeInfo\">FeeInfo</a>"],["impl CanonicalDeserialize for <a class=\"struct\" href=\"espresso_types/v0/v0_1/struct.NamespaceId.html\" title=\"struct espresso_types::v0::v0_1::NamespaceId\">NamespaceId</a>"],["impl CanonicalDeserialize for <a class=\"struct\" href=\"espresso_types/v0/v0_1/struct.Transaction.html\" title=\"struct espresso_types::v0::v0_1::Transaction\">Transaction</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[1011]}
+//{"start":57,"fragment_lengths":[904]}
