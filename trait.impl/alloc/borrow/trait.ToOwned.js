252bd072d11bd0490074a656457aab0a718d3255
pls148 <pls148@users.noreply.github.com>
2026-04-20 02:32:03 +0000

    deploy: 1eeb6e3708c3dcd9a0a869e6226f41b64fc53050

--- trait.impl/alloc/borrow/trait.ToOwned.js
+++ trait.impl/alloc/borrow/trait.ToOwned.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["espresso_types",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/alloc/borrow/trait.ToOwned.html\" title=\"trait alloc::borrow::ToOwned\">ToOwned</a> for <a class=\"struct\" href=\"espresso_types/v0/v0_1/struct.NsPayload.html\" title=\"struct espresso_types::v0::v0_1::NsPayload\">NsPayload</a>",0]]]]);
+    const implementors = Object.fromEntries([["espresso_types",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/alloc/borrow/trait.ToOwned.html\" title=\"trait alloc::borrow::ToOwned\">ToOwned</a> for <a class=\"struct\" href=\"espresso_types/v0/v0_1/struct.NsPayload.html\" title=\"struct espresso_types::v0::v0_1::NsPayload\">NsPayload</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- trait.impl/core/ops/arith/trait.AddAssign.js
+++ trait.impl/core/ops/arith/trait.AddAssign.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["node_metrics",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/ops/arith/trait.AddAssign.html\" title=\"trait core::ops::arith::AddAssign\">AddAssign</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.94.1/std/primitive.u64.html\">u64</a>&gt; for <a class=\"struct\" href=\"node_metrics/service/client_id/struct.ClientId.html\" title=\"struct node_metrics::service::client_id::ClientId\">ClientId</a>",0]]]]);
+    const implementors = Object.fromEntries([["node_metrics",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/ops/arith/trait.AddAssign.html\" title=\"trait core::ops::arith::AddAssign\">AddAssign</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.95.0/std/primitive.u64.html\">u64</a>&gt; for <a class=\"struct\" href=\"node_metrics/service/client_id/struct.ClientId.html\" title=\"struct node_metrics::service::client_id::ClientId\">ClientId</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
